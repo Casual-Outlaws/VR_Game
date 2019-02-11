6442c5d882c7b14bb7d81b15f005664ad7c8e857
DeltaTwoZero <deltatwozerosam@gmail.com>
2019-02-10 19:00:10 -0500

    Enemy AI v0.1
    -Uploaded first working AI

---
+++ SilentInTheDark/Assets/Scripts/Enemy.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    GameObject target, player;
+    NavMeshAgent agent;
+    [SerializeField]
+    AudioSource audioSource;
+    bool isMoving, isWaiting;
+    float timer;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        target = GameObject.FindGameObjectWithTag("Target");
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isWaiting = false;
+        isMoving = true;
+        timer = Random.Range(1, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWaiting)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                isMoving = true;
+                isWaiting = false;
+                timer = Random.Range(1, 5);
+            }
+        }
+
+        if(isMoving)
+        agent.destination = target.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Target")
+        {
+            isMoving = false;
+            int roll = Random.Range(0, 9);
+            if (roll >= 5)
+            {
+                isMoving = true;
+            }
+            else
+            {
+                isWaiting = true;
+            }
+        }
+    }
+
+
+}
---
+++ SilentInTheDark/Assets/Scripts/Target.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField]
+    GameObject targetPos;
+    [SerializeField]
+    int hits;
+    Vector3 newPos;
+
+
+    private void Awake()
+    {
+        targetPos = this.gameObject;
+        newPos = new Vector3(Random.Range(-8, 8), 1, Random.Range(-8, 8));
+        targetPos.transform.position = newPos;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        hits = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            print("hit");
+            newPos = new Vector3(Random.Range(-8, 8), 1, Random.Range(-8, 8));
+            targetPos.transform.position = newPos;
+            hits++;
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            newPos = new Vector3(Random.Range(-8, 8), 1, Random.Range(-8, 8));
+            targetPos.transform.position = newPos;
+            hits++;
+        }
+    }
+}
