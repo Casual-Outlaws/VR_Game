66f7c188a3eb0ecac7dad455e38147e96f919178
DeltaTwoZero <deltatwozerosam@gmail.com>
2019-02-21 22:10:46 -0500

    Target and Navmesh update
    - Added a dynamic navmesh for doors;
    - Target code now is more professional looking;
    - Added comments to the code;

--- SilentInTheDark/Assets/Scripts/Target.cs
+++ SilentInTheDark/Assets/Scripts/Target.cs
@@ -4,33 +4,27 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    [SerializeField]
-    GameObject targetPos;
-    [SerializeField]
-    int hits;
-    float timeReset;
-    Vector3 newPos;
+
+    [SerializeField] GameObject targetPos; //Object's position
+    [SerializeField] int hits; //Debug variable to check collision
+    float timeReset; //In case if the enemy can't reach this object for X seconds new position will be generated
+    Vector3 newPos; //new coordinates
 
 
     private void Awake()
     {
         targetPos = this.gameObject;
-        newPos = new Vector3(Random.Range(-15, 15), 1, Random.Range(-10, 20));
+        StartCoroutine("generatePosition");
         targetPos.transform.position = newPos;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
         hits = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
         timeReset += Time.deltaTime;
-        if (timeReset >= 15)
+        if (timeReset >= 10)
         {
-            newPos = new Vector3(Random.Range(-15, 15), 1, Random.Range(-10, 20));
+            StartCoroutine("generatePosition");
             targetPos.transform.position = newPos;
             timeReset = 0;
         }
@@ -41,21 +35,16 @@ public class Target : MonoBehaviour
         if (col.gameObject.tag == "Enemy")
         {
             print("hit");
-            newPos = new Vector3(Random.Range(-15, 15), 1, Random.Range(-10, 20));
+            StartCoroutine("generatePosition");
             targetPos.transform.position = newPos;
             hits++;
             timeReset = 0;
         }
     }
 
-    private void OnCollisionEnter(Collision col)
+    IEnumerator generatePosition()
     {
-        if (col.gameObject.tag == "Enemy")
-        {
-            newPos = new Vector3(Random.Range(-15, 15), 1, Random.Range(-10, 20));
-            targetPos.transform.position = newPos;
-            hits++;
-            timeReset = 0;
-        }
+        newPos = new Vector3(Random.Range(-8, 5), 1, Random.Range(-5, 10));
+        yield return null;
     }
 }
