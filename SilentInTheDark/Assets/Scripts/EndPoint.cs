b3a4cfa53917816a1df7455f4c6341f20acda821
hard-coded <cozyncoze@gmail.com>
2019-02-27 16:20:21 -0500

    added win condition

---
+++ SilentInTheDark/Assets/Scripts/EndPoint.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPoint : MonoBehaviour
+{
+    TextMesh textInfo;
+
+    private void Awake()
+    {
+        textInfo = GetComponentInChildren<TextMesh>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if( textInfo != null )
+        {
+            textInfo.text = string.Empty;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter( Collider other )
+    {
+        if( other.transform.root.tag == "Player" )
+        {
+            textInfo.text = "You Win!";
+            GameManager.Instance.gameState = GameState.WonGame;
+        }
+    }
+
+    private void OnTriggerExit( Collider other )
+    {
+        if( other.transform.root.tag == "Player" )
+        {
+            textInfo.text = string.Empty;
+        }
+    }
+}
---
+++ SilentInTheDark/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum GameState
+{
+    InMenu,
+    InTutorial,
+    InPlaying,
+    LostGame,
+    WonGame,
+};
+
+public class GameManager : MonoBehaviour
+{
+    public GameState gameState { get; set; }
+
+    private GameManager()
+    {
+
+    }
+    public static GameManager Instance { get; } = new GameManager();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameState = GameState.InPlaying;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
