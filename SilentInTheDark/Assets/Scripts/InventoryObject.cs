a2948fc5cba867942e3f30c87cddecd7b309f769
Ben <34342294+LeScoops@users.noreply.github.com>
2019-03-03 11:05:58 -0500

    Basic Implementation

---
+++ SilentInTheDark/Assets/Scripts/InventoryObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryObject : MonoBehaviour
+{
+    public GameObject heldObject;
+
+    void OnTriggerEnter(Collider other)
+    {
+        heldObject = other.gameObject;
+        other.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        heldObject = null;
+        other.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    void Update()
+    {
+        heldObject.gameObject.transform.position = this.transform.position;
+    }
+}
