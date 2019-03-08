c3fdd06eac14483da5d097c8caf4f79faadf2364
Ben <34342294+LeScoops@users.noreply.github.com>
2019-03-08 14:26:21 -0500

    Added Inventory Slot
    Notes:
    - Can run into object and it'll stick to it
    - Will only collect objects with tag "HoldableObject"
    - Each slot can only hold one object at a time
    - If slot is occupied and the object bumps into something, held object goes berserk

---
+++ SilentInTheDark/Assets/Scripts/InventoryObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryObject : MonoBehaviour
+{
+    public GameObject heldObject;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "HoldableObject" && !heldObject)
+        {
+            heldObject = other.gameObject;
+            other.GetComponent<Rigidbody>().useGravity = false;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "HoldableObject" && heldObject == other.gameObject)
+        {
+            heldObject = null;
+            other.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+
+    void Update()
+    {
+        heldObject.gameObject.transform.position = this.transform.position;
+    }
+}
