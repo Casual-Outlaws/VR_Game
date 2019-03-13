d2aaeba9776b247210178b06a12663f6287a03ba
Ben <34342294+LeScoops@users.noreply.github.com>
2019-03-13 11:04:57 -0400

    Added Ignore functionality to objects in inventory slot
    Also added golden objects

--- SilentInTheDark/Assets/Scripts/InventoryObject.cs
+++ SilentInTheDark/Assets/Scripts/InventoryObject.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class InventoryObject : MonoBehaviour
 {
     public GameObject heldObject;
+    GameObject otherObject;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "HoldableObject" && !heldObject)
+        if (other.gameObject.tag == "ItemD" && !heldObject)
         {
             heldObject = other.gameObject;
             other.GetComponent<Rigidbody>().useGravity = false;
+        }
+        else if (other.gameObject.tag == "ItemD" && heldObject)
+        {
+            Physics.IgnoreCollision(otherObject.GetComponent<Collider>(), heldObject.GetComponent<Collider>());
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "HoldableObject" && heldObject == other.gameObject)
+        if (other.gameObject.tag == "ItemD" && heldObject == other.gameObject)
         {
             heldObject = null;
             other.GetComponent<Rigidbody>().useGravity = true;
@@ -30,10 +35,11 @@ public class InventoryObject : MonoBehaviour
         {
             this.gameObject.GetComponent<MeshRenderer>().enabled = false;
             heldObject.gameObject.transform.position = this.transform.position;
+            heldObject.gameObject.transform.rotation = this.transform.rotation;
         }
         else
         {
-            this.gameObject.GetComponent<MeshRenderer>().enabled = true;
+            this.gameObject.GetComponent<MeshRenderer>().enabled = true;            
         }
     }
 }
