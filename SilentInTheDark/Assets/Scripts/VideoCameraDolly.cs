c5771ed375cb11a2e1f0c0ec3f2bfff0fbbddd59
Ben <34342294+LeScoops@users.noreply.github.com>
2019-03-14 14:14:04 -0400

    Alpha Video Sample Scene

---
+++ SilentInTheDark/Assets/Scripts/VideoCameraDolly.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoCameraDolly : MonoBehaviour
+{
+    public float speed = 0.1f;
+    public bool x;
+    //public bool z;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(x) transform.Translate(new Vector3(1,0,0) * Time.deltaTime * speed);
+        //if(z) transform.Translate(new Vector3(0,0,1) * Time.deltaTime * speed);
+    }
+}
