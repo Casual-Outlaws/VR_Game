de3b23274c7d640aa27676b09603cb3571a7c6f9
DeltaTwoZero <deltatwozerosam@gmail.com>
2019-03-28 00:25:56 -0400

    New mechanic for new enemy (WIP)
    - Added tracker for player # of teleports;
    - Added array to turn game objects' navmesh curve on/off;
    - Modified GameManager;

--- SilentInTheDark/Assets/Scripts/GameManager.cs
+++ SilentInTheDark/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 public enum GameState
@@ -17,6 +18,9 @@ public class GameManager : MonoBehaviour
     public GameState gameState { get; set; }
 
     public static GameManager Instance = null;
+
+    [SerializeField] int playerTPcount = 0;
+    [SerializeField] GameObject[] navmeshGO;
 
     private void Awake()
     {
@@ -41,6 +45,54 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerTPcount == 4)
+        {
+            if (navmeshGO != null)
+            {
+                for (int i = 0; i < navmeshGO.Length; i++)
+                {
+                    navmeshGO[i].GetComponent<NavMeshObstacle>().carving = false;
+                }
+
+                StartCoroutine("ResetNavMeshObstacles");
+            }
+            else
+            {
+                print("NavMesh Game Object array is NULL");
+            }
+        }
+    }
+
+    public void HeardSound(RoomEvent eventType, Vector3 location)
+    {
+        if (eventType == RoomEvent.OBJECT_THREW)
+        {
+            // start to investigate where the sound come
+            //Debug.LogFormat( "heard sound at {0}", location );
+        }
+        else if (eventType == RoomEvent.PLYAER_TELEPORTED)
+        {
+            // increment player teleportation count here
+            // ex. numPlayerTeleport += 1;
+            //Debug.LogFormat( "player teleport to {0}", location );
+
+            playerTPcount += 1;
+        }
+    }
+
+    IEnumerator ResetNavMeshObstacles()
+    {
+        float timer = 0;
+        timer += Time.deltaTime;
+        if(timer == 10f)
+        {
+            for (int i = 0; i < navmeshGO.Length; i++)
+            {
+                navmeshGO[i].GetComponent<NavMeshObstacle>().carving = true;
+            }
+            StopCoroutine("ResetNavMeshObstacles");
+        }
+            
+        yield return null;
     }
 }
